d94f66de531a50903bf9e279071511090ee0f17d
elsapeng <elsa.peng@hotmail.com>
2015-09-20 15:51:02 -0400

    first commit

---
+++ app/src/main/java/edu/upenn/cis573/travelingsalesman/Stroke.java
@@ -0,0 +1,17 @@
+package edu.upenn.cis573.travelingsalesman;
+
+import android.graphics.Point;
+import android.graphics.Color;
+
+import java.util.ArrayList;
+
+/**
+ * Created by linjie on 9/20/15.
+ */
+public class Stroke {
+    protected ArrayList<Point> coordinates = new ArrayList<Point>();
+    protected Color color;
+    protected int width;
+    protected boolean isValid;
+
+}
